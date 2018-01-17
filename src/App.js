3459fe2c8a7e1e20c5d89b966d3c7d7ee750ca12
db001 <dnbennett@hotmail.co.uk>
2018-01-17 13:12:12 +0000

    Finished updating state

--- src/App.js
+++ src/App.js
@@ -22,18 +22,41 @@ const list = [
 ];
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
   render() {
     return (
       <div className="App">
-        {list.map(item => 
+        {this.state.list.map(item => 
           (
-            <div key={item.objectID}> { /* Key added as recommended at https://reactjs.org/docs/lists-and-keys.html#keys */ }
+            <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
               </span>
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button">Dismiss
+                </button>
+              </span>
             </div>
           )
         )}
