a4a3bb68c02febaabec844efaa30f5e534c7a9c0
db001 <dnbennett@hotmail.co.uk>
2018-01-19 12:46:03 +0000

    Componenting Search and Table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Search from './components/Search';
+import Table from './components/Table';
+
 
 const list = [
   {
@@ -19,7 +22,27 @@ const list = [
     points: 5,
     objectID: 1,
   },
+  {
+    title: 'You don\'t know JS',
+    url: 'https://github.com/getify/you-dont-know-JS',
+    author: 'Kyle Simpson',
+    num_comments: 7,
+    points: 12, 
+    objectID: 2
+  },
+  {
+    title: 'JavaScript: Novice to Ninja',
+    url: 'https://www.jsnovicetoninja.com',
+    author: 'Darren Jones',
+    num_comments: 4,
+    points: 3,
+    objectID: 3
+  }
 ];
+
+const isSearched = searchTerm => item => 
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  
 
 class App extends Component {
 
@@ -28,49 +51,44 @@ class App extends Component {
 
     this.state = {
       list,
+      searchTerm: '',
     };
 
-    /* this.onDismiss = this.onDismiss.bind(this);  Not needed as using arrow function to define onDismiss */
-
+    /*
+    Not needed as using arrow function to define functions
+    this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    */
+    
   }
   
-  onDismiss = (id) => {
+  onDismiss = id => {
     const isNotId = item => item.objectID !== id;
     const updatedList = this.state.list.filter(isNotId);
     this.setState({ list: updatedList });
   }
 
+  onSearchChange = event => {
+    this.setState({ searchTerm: event.target.value })
+  }
+
   
 
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <form>
-          <input 
-            type="text"
-            placeholder="Search..."
-            onChange={this.onSearchChange} />
-        </form>
-        {this.state.list.map(item => 
-          (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  onClick={() => this.onDismiss(item.objectID)}
-                  type="button">
-                  Dismiss
-                </button>
-              </span>
-            </div>
-          )
-        )}
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+        
       </div>
     );
   }
